8b00e1f19d284bb400fa427f4b9bfe6785a9cdc3
Dawn <dawn.mott@oregonmetro.gov>
2019-11-19 11:01:30 -0800

    update indexjs isDisabled with empty object

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,8 @@
-import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
+import { empty } from '@ember/object/computed';
+
 
 export default Controller.extend({
-  isDisabled: true,
   emailAddress: '',
-  actualEmailAddress: computed('emailAddress', function() { 
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
-  emailAddressChanged: observer('emailAddress', function() { 
-    console.log('observer is called', this.get('emailAddress')); 
-  })
+  isDisabled: empty('emailAddress')
 });
