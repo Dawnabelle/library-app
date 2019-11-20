86a31ddc81392ac4f40e45d7e03027a0ae017867
dawnabelle <dawn.mott@gmail.com>
2019-11-20 10:41:23 -0800

    update invitation link with promise

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,9 +2,9 @@ import Controller from "@ember/controller";
 import { match, not } from "@ember/object/computed";
 
 export default Controller.extend({
+  headerMessage: "Libraries are cool",
   responseMessage: "",
   emailAddress: "",
-  headerMessage: "Coming Soon",
 
   isValid: match("emailAddress", /^.+@.+\..+$/),
   isDisabled: not("isValid"),
@@ -14,10 +14,10 @@ export default Controller.extend({
       const email = this.get("emailAddress");
 
       const newInvitation = this.store.createRecord("invitation", { email: email });
-      newInvitation.save();
-
-      this.set("responseMessage", `Thank you! We've saved your email address: ${this.get('emailAddress')}`);
-      this.set("emailAddress", " "); 
+      newInvitation.save().then(response => {
+        this.set("responseMessage", `Thank you! We've saved your email address with the ID: ${response.get('id')}`);
+        this.set("emailAddress", " "); 
+      });
     }
   }
 });
