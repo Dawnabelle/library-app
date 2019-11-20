681967845bf5ed603298d8af4f3f6c5509a45521
dawnabelle <dawn.mott@gmail.com>
2019-11-20 14:34:11 -0800

    update logic for new and index libraries

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.store.createRecord("library");
+  },
+  actions: {
+    saveLibrary(newLibrary) {
+      newLibrary.save().then(() => this.transitionTo("libraries"));
+    },
+    willTransition() {
+      // rollbackAttributes() removes the recofrd from the store if the model 'is new'
+      this.controller.get("model").rollbackAttributes();
+    }
+  }
 });
