5582ff5eacad3901d02e55a9038d73f7fb4beab6
dawnabelle <dawn.mott@gmail.com>
2019-11-20 16:01:41 -0800

    add email and messages to the database for contacts

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -4,21 +4,21 @@ import { match, not, and, gte } from "@ember/object/computed";
 export default Controller.extend({
   contactHeader: "Contact Us",
   contactResponseMessage: "",
-  emailAddress: "",
-  textContent: "",
+  email: "",
+  message: "",
 
-  isValidEmail: match("emailAddress", /^.+@.+\..+$/),
-  isMessageLongEnough: gte("contactText", /^\w+$/),
+  isValidEmail: match("email", /^.+@.+\..+$/),
+  isMessageLongEnough: gte("message", /^\w+$/),
 
   isValid: and("isValidEmail", "isMessageLongEnough"),
   isDisabled: not("isValid"),
 
   actions: {
     sendMessage() {
-      alert(`Saving your message ${this.get("contactText")} from ${this.get("emailAddress")}.`);
+      alert(`Saving your message ${this.get("message")} from ${this.get("email")}.`);
       this.set("contactResponseMessage", `Thank you! We've sent your message from ${this.get('emailAddress')}.`);
-      this.set("emailAddress", " ");
-      this.set("contactText", " "); 
+      this.set("email", " ");
+      this.set("message", " "); 
     }
   }
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.store.createRecord("contact");
+  },
+  actions: {
+    saveContact(newContact) {
+      newContact.save().then(() => this.transitionTo("contacts"));
+    },
+    willTransition() {
+      this.controller.get("model").rollbackAttributes();
+    }
+  }
 });
