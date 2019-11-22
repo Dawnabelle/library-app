20f643b0aa3302a042c82eb07993bfce0a10e1d0
dawnabelle <dawn.mott@gmail.com>
2019-11-22 12:12:04 -0800

    update library validation into model

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,11 @@
-import Model, { attr } from '@ember-data/model';
+// import Model, { attr } from '@ember-data/model';
+import DS from 'ember-data';
+import { notEmpty } from '@ember/object/computed';
 
-export default Model.extend({
-  name: attr("string"),
-  address: attr("string"),
-  phone: attr("string")
+export default DS.Model.extend({
+  name: DS.attr("string"),
+  address: DS.attr("string"),
+  phone: DS.attr("string"),
+
+  isValid: notEmpty("name")
 });
