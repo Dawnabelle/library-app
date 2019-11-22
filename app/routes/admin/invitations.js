a153eaba615966087258bc5d4a6e442f882be721
dawnabelle <dawn.mott@gmail.com>
2019-11-22 09:46:19 -0800

    add logic for delete invitations button

--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -2,6 +2,18 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+        // downloads single record from the server with the given id 
+    // the id comes from the url 
     return this.store.findAll("invitation");
+  },
+
+  actions: {
+    deleteInvitation(invitation) {
+      let confirmation = confirm("Are you sure?");
+      
+      if (confirmation) {
+        invitation.destroyRecord();
+      }
+    }
   }
 });
